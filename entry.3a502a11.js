78836c4f51da9cf55a80c7aaf144fa1858b89a32
gotomi <gotomi@users.noreply.github.com>
2022-04-04 09:19:27 +0000

    deploy: 2c1b7b6d6899bd028cf38d3c5b996d3996ad1534

---
+++ entry.3a502a11.js
@@ -0,0 +1,1 @@
+import"https://cdn.jsdelivr.net/npm/apexcharts";const a=document.getElementById("wrapper1"),r=document.getElementById("wrapper2");displayCharts("/data-chart/crux-origin-all.json","Origin",a);displayCharts("/data-chart/crux-home-all.json","Home Page",r);
---
+++ entry.9b613736.js
@@ -0,0 +1,1 @@
+const a=document.getElementById("wrapper1"),r=document.getElementById("wrapper2");displayCharts("/data-chart/crux-origin-all.json","Origin",a);displayCharts("/data-chart/crux-home-all.json","Home Page",r);
