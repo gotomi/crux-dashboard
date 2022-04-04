2c1b7b6d6899bd028cf38d3c5b996d3996ad1534
Tomasz Gospodarczyk <tomasz.gospodarczyk@allegro.pl>
2022-04-04 11:18:45 +0200

    migrate to astro 0.26

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,16 +5,8 @@ export default {
     // pages: './src/pages', // Path to Astro components, pages, and data
     // dist: './dist',       // When running `astro build`, path to final static output
     // public: './public',   // A folder of static files Astro will copy to the root. Useful for favicons, images, and other files that don’t need processing.
-    buildOptions: {
-        site: 'http://crux.gotomi.info', // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
-        sitemap: true, // Generate sitemap (set to "false" to disable)
-    },
-    devOptions: {
-        // hostname: 'localhost',  // The hostname to run the dev server on.
-        // port: 3000,             // The port to run the dev server on.
-        // tailwindConfig: '',     // Path to tailwind.config.js if used, e.g. './tailwind.config.js'
-    },
+    site: 'http://crux.gotomi.info', // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
     integrations: [
         svelte()
-      ]
+    ]
 };
