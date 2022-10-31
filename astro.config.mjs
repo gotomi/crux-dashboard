1c149b9c156248f977b90c41ac1edd85ace4ffc6
gotomi <gotomi@gotomi.info>
2022-10-31 22:29:40 +0100

    update, add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import svelte from '@astrojs/svelte';
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
-export default {
-    // projectRoot: '.',     // Where to resolve all URLs relative to. Useful if you have a monorepo project.
-    // pages: './src/pages', // Path to Astro components, pages, and data
-    // dist: './dist',       // When running `astro build`, path to final static output
-    // public: './public',   // A folder of static files Astro will copy to the root. Useful for favicons, images, and other files that don’t need processing.
-    site: 'http://crux.gotomi.info', // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
-    integrations: [
-        svelte()
-    ]
-};
+// https://astro.build/config
+export default defineConfig({
+  // projectRoot: '.',     // Where to resolve all URLs relative to. Useful if you have a monorepo project.
+  // pages: './src/pages', // Path to Astro components, pages, and data
+  // dist: './dist',       // When running `astro build`, path to final static output
+  // public: './public',   // A folder of static files Astro will copy to the root. Useful for favicons, images, and other files that don’t need processing.
+  site: 'http://crux.gotomi.info',
+  // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
+  integrations: [svelte(), sitemap()]
+});
