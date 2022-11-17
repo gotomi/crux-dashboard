12f4a47c28f8344b0be3f6dd44deea13f237388b
gotomi <gotomi@gotomi.info>
2022-11-17 23:26:26 +0100

    better chart page

---
+++ src/components/AllCharts.svelte
@@ -0,0 +1,47 @@
+<script>
+  import Chart from "./Chart.svelte";
+  export let data;
+  export let title;
+  let which = 0;
+  $: options = data[which];
+</script>
+
+<h2>{title}</h2>
+<p><strong>Choose metric:</strong></p>
+<ul>
+  {#each data as item, index}
+    <li>
+      <span
+        class={which === index ? "active" : ""}
+        on:click={() => (which = index)}>{item.title.text}</span
+      >
+    </li>
+  {/each}
+</ul>
+
+<div><Chart {options} /></div>
+
+<style>
+  ul,
+  li,
+  p {
+    list-style: none;
+    display: inline-block;
+    padding: 0 10px 0 0;
+  }
+
+  span {
+    background-color: antiquewhite;
+    display: inline-block;
+    padding: 5px 10px;
+    cursor: pointer;
+  }
+  .active {
+    background-color: coral;
+  }
+  div {
+    padding-bottom: 30px;
+    margin-bottom: 30px;
+    border-bottom: 1px solid #999;
+  }
+</style>
---
+++ src/components/Chart.svelte
@@ -0,0 +1,20 @@
+<script>
+  import { chart } from "svelte-apexcharts";
+
+  export let options = {
+    chart: {
+      type: "bar",
+    },
+    series: [
+      {
+        name: "sales",
+        data: [30, 40, 35, 50, 49, 60, 70, 91, 125],
+      },
+    ],
+    xaxis: {
+      categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999],
+    },
+  };
+</script>
+
+<div use:chart={options} />
